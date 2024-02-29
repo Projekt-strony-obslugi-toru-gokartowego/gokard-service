4d6da631751be206b01cf9547f0326a72666c9c1
Piotr <ptrpiw.dev@gmail.com>
2024-02-29 21:46:45 +0100

    Initial project commit

---
+++ src/main/java/com/gokart/gokartservice/GokartserviceApplication.java
@@ -0,0 +1,12 @@
+package com.gokart.gokartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokartserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokartserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gokart/gokartservice/GokartserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokart.gokartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokartserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
